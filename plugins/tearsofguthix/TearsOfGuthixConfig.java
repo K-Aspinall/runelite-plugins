0c54e4f27c1d9e8da7badd3bd7bfb4557cb21fc5
kristianaspinall <kristian@committed.io>
2019-10-01 14:49:07 +0100

    fixed description

--- plugins/tearsofguthix/TearsOfGuthixConfig.java
+++ plugins/tearsofguthix/TearsOfGuthixConfig.java
@@ -36,7 +36,7 @@ public interface TearsOfGuthixConfig extends Config
             position = 1,
             keyName = "showNextSkill",
             name = "Show the skill that will receive XP",
-            description = "Toggle the display of the skill that will receive XP"
+            description = "Toggle display of the skill that will receive XP"
     )
     default boolean showNextSkill()
     {
